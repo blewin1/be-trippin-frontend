a1d5be43c4e9361f427599443e7448010668d0a8
Antwain Hart <antwainhart@Antwains-MacBook-Pro.local>
2020-07-18 14:34:14 -0400

    stashing

---
+++ be-trippin-app/src/components/layout/About/About.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './footer.scss'
+
+const About = () => {
+    return (
+       <div className="awrap">
+           <h1>BeTrippin</h1>
+           <section className="first">
+           <p>BeTrippin is a new and improve roadtrip planner that will put all of your travel preparation concerns
+               at ease!
+           </p>
+           </section >
+           <aside className="second">
+               <p>Have you ever heard that "Proper, Preparation, Prevents, Poor, Performance"? Well thats
+               what BeTrippin does, with our customizable trip planner we Allow users to keep track 
+               of all aspects of there trip. Have you ever forgotten to pack your favorite 
+               pair of socks? Couldn't remember that one activity you wanted to do once you 
+               reached your destination? Simply input all of your travel requirements into BeTrippin
+               input fields and worry nomore!
+               </p>
+           </aside>
+           <section className="third">
+               <p>
+                   BeTrippin also allows users to track there every stop they may have along there BeTrippin
+                   aswell as flag your destination on our virtual map so you can see where your headed along 
+                   your route
+               </p>
+           </section>
+           <section className="forth">
+               <p>
+                   With BeTrippin we will turn your next roadtrip into the time of your life!
+               </p>
+           </section>
+
+       </div>
+    )
+}
+
+export default About
