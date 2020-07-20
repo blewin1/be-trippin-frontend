478ce4444040d9378640a12072c1da94e4193586
Antwain Hart <antwainhart@Antwains-MacBook-Pro.local>
2020-07-20 13:29:55 -0400

    about

--- be-trippin-app/src/components/routes/About/About.jsx
+++ be-trippin-app/src/components/routes/About/About.jsx
@@ -1,41 +1,44 @@
-import React from 'react'
-import '../About/about.scss'
+import React from "react";
+import "../About/about.scss";
 const About = () => {
-    return (
-        <div className="awrap">
-            <body>
-                <div id="bg"></div>
-                <div className="logo">
-            <h1>BeTrippin</h1>
-            </div>
-            <section className="first">
-                <p>BeTrippin is a new and improve roadtrip planner that will put all of your travel preparation concerns
-                at ease!
-           </p>
-            </section >
-            <section className="second">
-                <p>
-                Have you ever heard that "Proper, Preparation, Prevents, Poor, Performance"? Well thats
-                what BeTrippin does, with our customizable trip planner we Allow users to keep track
-                of all aspects of there trip. Have you ever forgotten to pack your favorite
-                pair of socks? Couldn't remember that one activity you wanted to do once you
-                reached your destination? Simply input all of your travel requirements into BeTrippin
-                input fields and worry nomore!
-               </p>
-            </section>
-            <section className="third">
-                <p>
-                   BeTrippin also allows users to track there every stop they may have along there BeTrippin
-                    aswell as flag your destination on our virtual map so you can see where your headed along
-                    your route 
-               </p>
-            </section>
-            <section className="forth">
-                <p>
-                    With BeTrippin we will turn your next roadtrip into the time of your life!
-               </p>
-            </section>
-            {/* <div className="container line1">
+  return (
+    <div className="awrap">
+      <body>
+        <div id="bg"></div>
+        <div className="logo">
+          <h1>BeTrippin</h1>
+        </div>
+        <section className="first">
+          <p>
+            BeTrippin is a new and improve roadtrip planner that will put all of
+            your travel preparation concerns at ease!
+          </p>
+        </section>
+        <section className="second">
+          <p>
+            Have you ever heard that "Proper, Preparation, Prevents, Poor,
+            Performance"? Well thats what BeTrippin does, with our customizable
+            trip planner we Allow users to keep track of all aspects of there
+            trip. Have you ever forgotten to pack your favorite pair of socks?
+            Couldn't remember that one activity you wanted to do once you
+            reached your destination? Simply input all of your travel
+            requirements into BeTrippin input fields and worry nomore!
+          </p>
+        </section>
+        <section className="third">
+          <p>
+            BeTrippin also allows users to track every stop they may have along
+            there BeTrippin aswell as flag your destination on our virtual map
+            so you can see where your headed along your route
+          </p>
+        </section>
+        <section className="forth">
+          <p>
+            With BeTrippin we will turn your next roadtrip into the time of your
+            life!
+          </p>
+        </section>
+        {/* <div className="container line1">
                
                     <div className="col-6 justify-content ">
                         <div class="card ">
@@ -60,14 +63,12 @@ const About = () => {
 
                 </div>
                 <div className="col-5 justify-content "> */}
-      
 
-
-                {/* </div>
+        {/* </div>
             </div> */}
-            </body>
-        </div>
-    )
+      </body>
+    </div>
+  );
 };
 
-export default About
+export default About;
