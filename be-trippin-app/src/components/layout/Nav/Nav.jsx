f5c391a44e4926e586700e4a381b393a566e5121
Zhijie Xu <jennyxu000@gmail.com>
2020-07-22 16:04:50 -0700

    basic nav bar done

--- be-trippin-app/src/components/layout/Nav/Nav.jsx
+++ be-trippin-app/src/components/layout/Nav/Nav.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./nav.scss";
 
 const Nav = () => {
-    return (
-        <nav>
-            <h2>Nav</h2>
-        </nav>
-    )
-}
+  const [active, setActive] = useState(false);
+  const handleClick = () => {
+    setActive(!active);
+  };
+  return (
+    <div className="topnav">
+      <div className="hamlogo">
+        <div
+          className={`hamburger ${active ? "change" : ""}`}
+          onClick={handleClick}
+        >
+          <div className="bar1"></div>
+          <div className="bar2"></div>
+          <div className="bar3"></div>
+        </div>
+        <NavLink to="/" className="logopic" onClick={() => setActive(!active)}>
+          <img src="https://res.cloudinary.com/headincloud/image/upload/v1595455957/betrippinlogo.png" />
+        </NavLink>
+      </div>
 
-export default Nav
+      <nav className={`nav ${active ? "shownav" : "hidenav"}`}>
+        <NavLink to="/" onClick={() => setActive(!active)}>
+          Trips
+        </NavLink>
+        <NavLink to="/team" onClick={() => setActive(!active)}>
+          Team
+        </NavLink>
+        <NavLink to="/about" onClick={() => setActive(!active)}>
+          About
+        </NavLink>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
