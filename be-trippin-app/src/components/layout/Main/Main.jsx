ec0f61ecc4ac9b9b8bd7b6a51dcb6816037bba6d
Natasha Sana <nfsana@gmail.com>
2020-07-22 08:39:00 -0700

    synced timer to backend

--- be-trippin-app/src/components/layout/Main/Main.jsx
+++ be-trippin-app/src/components/layout/Main/Main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route, Redirect} from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "../../routes/Home/Home";
 import TeamPage from "../../routes/TeamPage/TeamPage";
 import About from "../../routes/About/About";
